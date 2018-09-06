8bcee300e49c8bd61b5a1cf65bbb3c77b508fb66
loxstomper <lochieashcroft@gmail.com>
2018-09-06 15:53:09 +1000

    added files

---
+++ app.js
@@ -0,0 +1,37 @@
+var create  = require('./create');
+var add     = require('./add');
+var remove  = require('./remove');
+var update  = require('./update');
+var read    = require('./read');
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
+
+
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  insertDocuments(db, function() {
+    client.close();
+  });
+});
+
